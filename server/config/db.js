845a2ecabe5ff4b46e550b1debbfc624e5213ead
officialaditshrm <officialaditshrm@gmail.com>
2025-06-27 00:08:19 +0530

    security measures

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://officialaditshrm:resumebuilderpassword@cluster0.o8t53u0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.DB_URL)
         console.log("Database connection Successful")
     } catch (error) {
         console.log("Database connection Failed")

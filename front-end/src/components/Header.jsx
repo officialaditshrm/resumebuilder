dc0b9a3c38c657f418622df186ec1905bac52dad
officialaditshrm <officialaditshrm@gmail.com>
2025-06-27 11:56:31 +0530

    branding

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header ({setDarkMode, darkMode, loggedInUser, smallScreen, hamburgerOpe
     if (!smallScreen) {
         return (
             <header className = "z-40 fixed right-5 top-3 h-20 lg:w-[540px] md:w-[480px] dark:bg-zinc-800 bg-neutral-200 flex justify-between items-center rounded-xl shadow-[0_2px_5px_1px_rgba(0,0,0,0.25)]">
-                <img src = '/logotransparent.png' className = " object-contain w-[20%] p-4"/>
+                <img src = {darkMode ? "/logotransparentdark.png": "/logotransparent.png"} className = " object-contain w-[20%] p-4"/>
                 <div className = "flex relative flex-1 justify-evenly overflow-hidden">
                     <Link title='My Resumes' to = "/" className = "hover:scale-[0.9] w-1/4 flex justify-center items-center"><img className = "size-3/5" src = {darkMode? "/editdoc.svg" : "/editdocblack.svg"} /></Link>
                     <Link title = "Community" to = "/community" className = "hover:scale-[0.9] w-1/4 flex justify-center items-center"><img className = "size-3/5" src = {darkMode? "/community.svg" : "/communityblack.svg"} /></Link>
